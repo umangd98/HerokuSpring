387b1bfdc40251387d8cb5f2f9a4a338aad7152d
umangd98 <umangd98@gmail.com>
2020-02-22 01:29:04 +0530

    react

---
+++ src/main/resources/static/precache-manifest.c47bf6acbccaaa601106641ee8e61280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccd205301367e3fa41c25521eb090f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929e0734d7420dd3a81d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "14d50cc96fe643bc8498",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "929e0734d7420dd3a81d",
+    "url": "/static/js/2.f6390147.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.f6390147.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14d50cc96fe643bc8498",
+    "url": "/static/js/main.f7ec14f7.chunk.js"
+  },
+  {
+    "revision": "8677cb06ee07da3e0258",
+    "url": "/static/js/runtime-main.f12de98b.js"
+  }
+]);
